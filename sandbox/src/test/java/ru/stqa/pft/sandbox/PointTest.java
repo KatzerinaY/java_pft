721e91aa1e908f47344f6adbbe00a041b2479e68
Yarush <kateryna.yarush@outlook.com>
2021-05-09 10:59:25 +0200

    homework 3 PointTest

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
@@ -0,0 +1,20 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.annotations.Test;
+
+
+public class PointTest {
+    @Test
+    public void testDistance1(){
+        Point p1 = new Point(3,5);
+        Point p2 = new Point(6,5);
+        assert p1.distance(p2) == 3;
+    }
+    @Test
+    public void testDistance2(){
+        Point p1 = new Point(3,7);
+        Point p2 = new Point(3,5);
+        assert p2.distance(p1) == 2;
+    }
+
+}
