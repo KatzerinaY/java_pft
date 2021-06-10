ae67721dd727676c53d812fb254fc5b13dda811b
Yarush <kateryna.yarush@outlook.com>
2021-06-10 16:42:32 +0200

    homework 14

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -5,17 +5,14 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
-import java.util.Properties;
+import java.io.IOException;
 
 public class TestBase {
 
-    protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser",BrowserType.FIREFOX));
-    Properties test = System.getProperties();
-//    protected static final ApplicationManager app = new ApplicationManager("firefox");
+    protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser",BrowserType.CHROME));
 
     @BeforeSuite
-    public void setUp()
-    {
+    public void setUp() throws IOException {
         app.init();
     }
 
