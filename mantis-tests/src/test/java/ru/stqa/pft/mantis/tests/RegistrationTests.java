b46cfddaa8168fa9e08707fdc60e4913f173938d
Yarush <kateryna.yarush@outlook.com>
2021-06-18 17:03:15 +0200

    james mail server

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 public class RegistrationTests extends TestBase {
 
-    @BeforeMethod
+    //@BeforeMethod   //- OFF  for James // ON for integrated mail server
     public void startMailServer(){
         app.mail().start();
     }
@@ -22,11 +22,17 @@ public class RegistrationTests extends TestBase {
     @Test
     public  void  testRegistration() throws IOException, MessagingException {
         long now = System.currentTimeMillis();
-        String email = String.format("user%s@localhost.localadomain",now);
         String user = String.format("user%s",now);
         String password = "password";
+        // email = String.format("user%s@localhost.localadomain",now); // for local mail server
+        String email = String.format("user%s@localhost",now); //for james mail server
+
+        app.james().createUser(user,password);
+
         app.registration().start(user, email);
-        List<MailMessage>mailMessages = app.mail().waitForMail(2,10000);
+        //List<MailMessage>mailMessages = app.mail().waitForMail(2,10000);//for local mail server
+        List<MailMessage>mailMessages = app.james().waitForMail(user,password,60000); //for james mail server
+
         String confirmationLink = findConfirmationLink(mailMessages,email);
         app.registration().finish(confirmationLink, password, user);
 
@@ -39,7 +45,7 @@ public class RegistrationTests extends TestBase {
         return regex.getText(mailMessage.text);
     }
 
-    @AfterMethod(alwaysRun = true)
+    //@AfterMethod(alwaysRun = true) //- OFF  for James // ON for integrated mail server
     public void stopMailServer(){
         app.mail().stop();
     }
