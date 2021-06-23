9767dacbd0bbd3d55f212126dd6c85afda001eac
Yarush <kateryna.yarush@outlook.com>
2021-06-23 10:39:57 +0200

    sandbox neu print

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
@@ -9,12 +9,14 @@ public class PointTest {
         Point p1 = new Point(3,5);
         Point p2 = new Point(6,5);
         assert p1.distance(p2) == 3;
+        System.out.println("test1 katja");
     }
     @Test
     public void testDistance2(){
         Point p1 = new Point(3,7);
         Point p2 = new Point(3,5);
         assert p2.distance(p1) == 2;
+        System.out.println("test2 katja");
     }
 
 }
